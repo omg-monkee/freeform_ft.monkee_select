f65bcfad362c6604e3d3af25d7f6e574892da56d
Monk-ee <john@omahamediagroup.com>
2016-10-20 15:16:46 -0500

    Create lang.monkee_select.php

---
+++ system/user/addons/monkee_select/language/english/lang.monkee_select.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Monk-ee Select - Language
+ */
+
+$lang = array(
+//Field Info
+'monksel_default_field_name' => "Monk-ee Select",
+'monksel_default_field_desc' => "Modified version of the default Select fieldtype.",
+'monksel_default_field_type' => "monkee_select",
+
+//Settings
+'monksel_blank_line' => "Insert Blank Option",
+'monksel_blank_line_desc' => "Check this box to add a blank line option to the top of the select box. This will be the default option if no value is entered in the Default Option box below.",
+'monksel_default_value' => "Default Value",
+'monksel_default_value_desc' => "If form data is not being edited or there is no saved data, this option will be selected by default. For a Value/Label list, enter the value. NOTE: Setting the inline 'default_value' parameter will override these settings.",
+'monksel_enable_autofocus' => "Enable Autofocus",
+'monksel_enable_autofocus_desc' => "Field will automatically be selected on page load. If multiple fields on a form have this property enabled, the field loaded first will claim control.",
+'monksel_enable' => "Enable",
+'monksel_field_title' => "Populate Title Parameter",
+'monksel_field_title_desc' => "Select if and how you want to populate the title field in the input tag. This is useful for displaying tooltips. NOTE: Setting the inline 'attr:title' parameter will override these settings.",
+'monksel_field_none' => "None",
+'monksel_field_label' => "Field Label",
+'monksel_field_desc' => "Field Description",
+'monksel_field_custom' => "Custom",
+'monksel_field_title_custom' => "Enter custom title here",
+'monksel_css_classes' => "CSS Classes",
+'monksel_css_classes_desc' => "Output as many classes as you like. The checkboxes will add dynamic information as classes. Enter any custom classes in the text box. NOTE: Setting the inline 'attr:class' parameter will override these settings. Field Name pulls from the Field Name value shown above. Freeform Field Type will output 'monkee_select'",
+'monksel_css_name' => "Field Name",
+'monksel_css_type' => "Freeform Field Type",
+'monksel_css_custom' => "Enter custom css styles here",
+'monksel_custom_params' => "Custom Parameters",
+'monksel_custom_params_desc' => "Enter custom parameters and values here. Values not required. These will be added to the field tag.",
+'monksel_custom_place' => "Parameter",
+'monksel_custom_value' => "Value",
+'monksel_js_event' => "Custom Javascript/jQuery Event",
+'monksel_js_event_desc' => "For advanced users only. Select a javascript event from the dropdown box. Then, in the textbox, enter the javascript (or jQuery) you would like to attach to this event. <b>PLEASE NOTE: Because of the way Freeform handles field entry on this page, you must escape parentheses with a backslash. Failing to do this will result in this form not saving. Also, DO NOT USE DOUBLE QUOTES, only single quotes. Ex: alert&#92;('test'&#92;);.</b> <a href='http://www.w3schools.com/jsref/dom_obj_event.asp' target='_blank'>Click here to view a list of javascript events and descriptions.</a> NOTE: Setting the inline 'attr:onclick' parameter will override an onclick action setup here.",
+'monksel_js_action' => "Enter JavaScript/jQuery action here",
+'monksel_js_confirm' => "I confirm that I am an advanced user and have read the instructions.",
+
+//END
+'' => ''
+);
